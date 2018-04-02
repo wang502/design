47d537c03173140f0bf8472603f1ded5e1555f22
Xiaohui Wang <wang502@indiana.edu>
2018-04-02 17:33:12 -0400

    modified formatter

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -12,9 +12,3 @@ type FuncFormatter func(space []byte, kv []interface{}) []byte
 func (f FuncFormatter) Format(space []byte, kv []interface{}) []byte {
 	return f(space, kv)
 }
-
-type OtherFormatter func(space []byte, kv []interface{}) []byte
-
-func (o OtherFormatter) Format(space []byte, kv []interface{}) []byte {
-	return o(space, kv)
-}
